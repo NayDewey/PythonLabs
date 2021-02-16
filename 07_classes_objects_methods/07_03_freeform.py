74571e442a824bc0e6cc34d6fc5579fdcb9cb4fd
Nay Dewey <naydewey@nays-mbp.lan>
2021-02-16 16:35:31 -0500

    Initial commit: Add CodingNomads Online Labs

---
+++ 07_classes_objects_methods/07_03_freeform.py
@@ -0,0 +1,15 @@
+'''
+- Write a script with three classes that model everyday objects.
+- Each class should have an __init__ method that sets at least three attributes
+- Include a __str__ method in each class that prints out the attributes
+    in a nicely formatted string.
+- Overload the __add__ method in one of the classes so that it's possible
+    to add attributes of two instances of that class using the + operator.
+- Create at least two instances of each class.
+- Once the objects are created, change some of their attribute values.
+
+Be creative. Have some fun. :)
+Using objects you can model anything you want.
+Cars, animals, card games, sports teams, trees, people etc...
+
+'''
---
+++ 10_testing/10_01_unittest.py
@@ -0,0 +1,12 @@
+'''
+Demonstrate your knowledge of unittest by first creating a function with input parameters and a return value.
+
+Once you have a function, write at least two tests for the function that use various assertions. The
+tests should pass. Also include a test that does not pass.
+
+NOTE: You can write both the code as well as the tests for it in this single file.
+However, feel free to adhere to best practices and separate your tests and the functions you are testing
+into different files. Note that you will run into an error when attempting to import this file,
+because Python modules can't begin with a number.
+
+'''
