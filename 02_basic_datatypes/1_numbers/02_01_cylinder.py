bd2db1d8605c57ceb6c0f0e3bb3eeda5f64c8b47
naydewey <mrs.lynne.dewey@gmail.com>
2021-02-24 20:06:28 -0500

    All labs updated to reflect the correct calculations section 2

--- 02_basic_datatypes/1_numbers/02_01_cylinder.py
+++ 02_basic_datatypes/1_numbers/02_01_cylinder.py
@@ -4,3 +4,17 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+
+#pi is 3.14159
+#volume of a cylinder is V = (pi)r2h where r is the radius and h is height
+#lateral surface area is 2(pi)radius(height)
+
+
+pi = 3.14159
+radius = 3.14
+height = 5
+volume = pi * radius * 2 * height
+lateral_surface_area = 2 * pi * radius * height
+
+print(volume)
+print(lateral_surface_area)
--- 02_basic_datatypes/1_numbers/02_03_runner.py
+++ 02_basic_datatypes/1_numbers/02_03_runner.py
@@ -4,3 +4,12 @@ If a runner runs 10 miles in 30 minutes and 30 seconds,
 What is his/her average speed in kilometers per hour? (Tip: 1 mile = 1.6 km)
 
 '''
+#10 miles is 16 kilometers, need to find the average time to complete 1 mile for the runner and convert
+#running pace is run time divided by distance
+
+run_time = 30.3 # minutes and seconds it took to run the 10 miles
+run_distance_kms = 16 # I converted this outside of the calculation - see above but I could include it easily
+
+pace_in_kms = run_time / run_distance_kms
+
+print(pace_in_kms)
