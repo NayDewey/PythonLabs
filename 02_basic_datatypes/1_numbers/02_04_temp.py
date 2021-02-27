13f6d2cc9c4faf5aaeb2ef2e276ebc37e426faf1
naydewey <mrs.lynne.dewey@gmail.com>
2021-02-27 03:39:04 -0500

    All labs updated to reflect the correct calculations section 2

--- 02_basic_datatypes/1_numbers/02_04_temp.py
+++ 02_basic_datatypes/1_numbers/02_04_temp.py
@@ -13,9 +13,10 @@ Output should read like - "81.32 degrees fahrenheit = 27.4 degrees celsius"
 
 # I do not understand 'read...from the conole' but I am interrupting that as 'user input' (???)
 # if that is not the case, that is fine, I just need to know what application or program you want to have the program gather the data from
-# barring that, I am just going to convert the degrees so that a user input freezing to boiling gives you a response
+
 
 request = int(input("Please enter the degree in Fahrenheit you would like to convert: "))
 celsius = ((request - 32) * (5 / 9))
+farenheit = request
+print(celsius, "degrees C =", farenheit, "degrees F")
 
-print(celsius)
